ab6d4e7e41f1de9c9b3817f03c0c0375ed1febcc
KazMac <karen.mackay.ic@uhi.ac.uk>
2019-09-19 15:39:07 +0100

    add string representation to enum

--- CodePeg.java
+++ CodePeg.java
@@ -1,10 +1,34 @@
+/**
+ * CodePeg
+ * @author Karen
+ * @version 1
+ * Desc: colored peg used in code
+ */
 package mastermind;
 
 public enum CodePeg {
-    RED,
-    BLUE,
-    GREEN,
-    WHITE,
-    BLACK,
-    YELLOW
+    RED("red"),
+    BLUE("blue"),
+    GREEN("green"),
+    WHITE("white"),
+    BLACK("black"),
+    YELLOW("yellow")
+
+    private String codePegString;
+
+    /*
+     * Intialise code peg colour string
+     * @param codePegColour colour of code peg
+     * */
+    CodePeg(String codePegString){
+        this.codePegString = codePegString;
+    }
+
+    /*
+     * @return peg colour as a string
+     * */
+    public String toString(){
+        return codePegString;
+    }
 }
+
